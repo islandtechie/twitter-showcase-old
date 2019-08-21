ecad4fe60184b41b2edb5ac7fdb3cbdf047416b0
Berlin Smith <berlin.smithjr@gmail.com>
2019-08-20 22:16:20 -0400

    Completed markup for home page
    deleted file not needed and completed markup for home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <img src="_assets/t-logo.png" alt=""/>
+      </div>
+      <div className="search-area">
+        <div className="user-search tweet">
+          <div className="tweet_avatar">
+            <img src="_assets/t-logo.png" alt=""/>
+          </div>
+          <div className="tweet__body">
+            <div className="tweet_body_header">
+              <span className="tweet__username">SearchForATweet</span>
+              <span className="tweet_handle">@SearchForATweet</span>
+              <span className="tweet_date">24 May, 2019</span>
+            </div>
+            <div className="tweet_body_text">
+              <p>Use the Search box below to search for a tweet from someone you follow! You can search by <em>@twitter_handle</em> or <em>tweet content</em>.</p>
+              <input type="text" id="user-search"/>
+            </div>
+          </div>
+        </div>
+        <div className="random-search">
+        <div className="tweet_avatar">
+            <img src="_assets/t-logo.png" alt=""/>
+          </div>
+          <div className="tweet__body">
+            <div className="tweet_body_header">
+              <span className="tweet__username">RandomTweet</span>
+              <span className="tweet_handle">@SearchARandomTweet</span>
+              <span className="tweet_date">24 May, 2019</span>
+            </div>
+            <div className="tweet_body_text">
+              <p>Use the Grab-A-Tweet button to search for a random tweet from people we follow! <em>#InspirationLaughsSelfDevlopment</em></p>
+              <button type="button" id="random-search">Grab-A-Tweet</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
