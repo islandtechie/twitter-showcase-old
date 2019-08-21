e2c7a9b4882a4157d2d91eba3fe8133bc8e57da5
Berlin Smith <berlin.smithjr@gmail.com>
2019-08-20 22:57:00 -0400

    Completed basic styling for home page
    will work on look at a later time

--- src/App.js
+++ src/App.js
@@ -15,24 +15,24 @@ function App() {
           <div className="tweet__body">
             <div className="tweet_body_header">
               <span className="tweet__username">SearchForATweet</span>
-              <span className="tweet_handle">@SearchForATweet</span>
-              <span className="tweet_date">24 May, 2019</span>
+              <span className="tweet__handle">@SearchForATweet</span>&#8226;
+              <span className="tweet__date">24 May, 2019</span>
             </div>
             <div className="tweet_body_text">
               <p>Use the Search box below to search for a tweet from someone you follow! You can search by <em>@twitter_handle</em> or <em>tweet content</em>.</p>
-              <input type="text" id="user-search"/>
+              <input type="text" id="user-search" placeholder="@elonmusk or 'bananas"/>
             </div>
           </div>
         </div>
-        <div className="random-search">
-        <div className="tweet_avatar">
+        <div className="random-search tweet">
+          <div className="tweet_avatar">
             <img src="_assets/t-logo.png" alt=""/>
           </div>
           <div className="tweet__body">
             <div className="tweet_body_header">
               <span className="tweet__username">RandomTweet</span>
-              <span className="tweet_handle">@SearchARandomTweet</span>
-              <span className="tweet_date">24 May, 2019</span>
+              <span className="tweet__handle">@SearchARandomTweet</span>&#8226;
+              <span className="tweet__date">24 May, 2019</span>
             </div>
             <div className="tweet_body_text">
               <p>Use the Grab-A-Tweet button to search for a random tweet from people we follow! <em>#InspirationLaughsSelfDevlopment</em></p>
