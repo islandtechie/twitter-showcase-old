9aefb2a6b2ac7e8c630e3aaffa1c736053e455aa
Berlin Smith <berlin.smithjr@gmail.com>
2019-08-22 20:56:30 -0400

    Created User and Random Search Pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../src/components/pages/Home';
+import Home from '../src/components/pages/Home';
 
 function App() {
   return (
