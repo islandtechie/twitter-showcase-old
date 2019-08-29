40befd1c26c2de8473d5790d2c510c7f089f6cd7
Berlin Smith <berlin.smithjr@gmail.com>
2019-08-29 06:02:50 -0400

    refactor App.js into class
    changed app.j to hold globl state

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../src/components/pages/Home';
@@ -6,7 +6,21 @@ import UserSearch from '../src/components/pages/UserSearch';
 import RandomSearch from '../src/components/pages/RandomSearch';
 import NavBar from './components/NavBar';
 
-function App() {
+class App extends Component {
+  state = {
+    searchText:''
+  }
+  render() {
+    return (
+      <Router>
+        <NavBar />>
+      </Router>
+    );
+  }
+}
+
+export default App;
+/* function App() {
   return (
     <Router>
       <NavBar />
@@ -17,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
